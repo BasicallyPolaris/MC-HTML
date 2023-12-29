63ada06aa9b8445a548b47f336a0009c44c01ede
Polaris <dfgxecrone@gmail.com>
2023-12-30 00:42:03 +0100

    Add favicon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@ const inputImage = $("#file-upload");
 inputImage.on("change", handleUpload);
 $("#generate-btn").on("click", generatePuzzle);
 
+// Handles the upload of an image
 function handleUpload() {
     $("#puzzle").attr("src", URL.createObjectURL(inputImage.prop("files")[0]));
     $("#puzzle").removeClass("d-none");
@@ -10,5 +11,5 @@ function handleUpload() {
 }
 
 function generatePuzzle() {
-    
+    const tiles = $("#tileOptions").find(":selected").val();
 }
